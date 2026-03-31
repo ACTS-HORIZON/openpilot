dd8c51f0170f254862857bc7ace54782731b9fdd
ACTS-HORIZON <42858845+ACTS-HORIZON@users.noreply.github.com>
2026-03-30 19:16:17 -0500

    Update latcontrol_torque.py

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -22,16 +22,16 @@ from openpilot.sunnypilot.selfdrive.controls.lib.latcontrol_torque_ext import La
 # Additionally, there is friction in the steering wheel that needs
 # to be overcome to move it at all, this is compensated for too.
 
-KP = 0.8
+KP = 0.6
 KI = 0.15
 
 INTERP_SPEEDS = [1, 1.5, 2.0, 3.0, 5, 7.5, 10, 15, 30]
 KP_INTERP = [250, 120, 65, 30, 7.0, 3.5, 2.5, 1.5, KP]
 
-LP_FILTER_CUTOFF_HZ = 1.2
-MEASUREMENT_LP_CUTOFF_HZ = 2.5
+LP_FILTER_CUTOFF_HZ = 1.0
+MEASUREMENT_LP_CUTOFF_HZ = 2.2
 JERK_LOOKAHEAD_SECONDS = 0.19
-JERK_GAIN = 0.3
+JERK_GAIN = 0.2
 LAT_ACCEL_REQUEST_BUFFER_SECONDS = 1.0
 VERSION = 1
 
